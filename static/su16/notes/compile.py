44f19d6ee21a8f8c9506d86a3dcb0044c6608aaf
Fan Ye <yefan1994@gmail.com>
2016-05-25 11:15:35 -0700

    finish staff page

---
+++ static/su16/notes/compile.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+def do_one(fname_base,directory):
+    os.chdir(directory)
+    os.system("pwd")
+    os.system("pdflatex " + fname_base + ".tex")
+    os.system("pdflatex " + fname_base + ".tex")
+    print("cp  " + fname_base + ".pdf ../compiled/")
+    os.system("cp  " + fname_base + ".pdf ../compiled/")
+    os.chdir("..")
+
+import glob
+
+files = glob.glob("./note*")
+print files
+
+def find_dir(name):
+    for string in files:
+        if name in string:
+            return string
+
+if (len(sys.argv) == 1):
+    os.system("mkdir compiled")
+    for x in xrange(0,23):
+        fname_base = "n" + str(x)
+        lookup_name = "./note" + str(x)
+        directory = find_dir(lookup_name)
+        do_one(fname_base, str(directory))
+
+else:
+    do_one(sys.argv[1],sys.argv[2])
+
+#print sys.argv[1],sys.argv[2]
